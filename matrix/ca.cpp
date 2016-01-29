4fe2773be444ac89ea6e6cacca07bdc9c64fa93e
willinss <hpgyw_008@163.com>
2016-01-29 18:33:23 +0800

    Kalman in x and y

--- matrix/ca.cpp
+++ matrix/ca.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 using namespace Eigen;
 using namespace cv;
-const int winHeight = 600;
-const int winWidth = 800;
+const int winHeight = 300;
+const int winWidth = 400;
 CvPoint mousePosition = cvPoint(winWidth >> 1, winHeight >> 1);
 void mouseEvent(int event, int x, int y, int flags, void *param)
  {
@@ -24,34 +24,55 @@ int main()
     cvSetMouseCallback("kalman", mouseEvent);
     IplImage* img = cvCreateImage(cvSize(winWidth, winHeight), 8, 3);
 
-    MatrixXd A(6,6);
-    A << 1, 0, 1, 0, 1, 0,
-         0, 1, 0, 1, 0, 1,
-         0, 0, 1, 0, 1, 0,
-         0, 0, 0, 1, 0, 1,
-         0, 0, 0, 0, 1, 0,
-         0, 0, 0, 0, 0, 1;
+    MatrixXd A_x(2,2);
+    A_x << 1, 0.55,
+		   0, 1;   
+    VectorXd predict_x_x(2);//state
+    predict_x_x(0) = winWidth >> 1;
+	predict_x_x(1) = 0;
+    VectorXd optimize_x_x(2);
+    RowVector2d H_x; //observation
+    H_x(0) = 1;
+	H_x(1) = 0;
+    MatrixXd Q_x(2,2); //process_noise
+    Q_x(0,0) = Q_x(1,1) = 1e-2;
+    double R_x; //measure_noise
+    R_x = 1e-1;
     
-    VectorXd predict_x(6);//state
-    predict_x(0) = winWidth >> 1;
-    predict_x(1) = winHeight >> 1;
-    VectorXd optimize_x(6);
-    MatrixXd H(2,6); //observationI
-    H << 1, 0, 0, 0, 0, 0,
-         0, 1, 0, 0, 0, 0;
-    MatrixXd Q(6,6); //process_noise
-    Q(0,0)=Q(1,1)=Q(2,2)=Q(3,3)=Q(4,4)=Q(5,5)=1e-3;
-    Matrix2d R; //measure_noise
-    R(0,0)=R(1,1)=1e-1;
-    
-    MatrixXd P(6,6); //cov
-    Matrix2d S; //temp matrix
-    MatrixXd K(6,2); //
-    Vector2d z; //measure value
-    Vector2d y;
-    MatrixXd I(6,6);
-    I = MatrixXd::Identity(6,6);
+    MatrixXd P_x(2,2); //cov
+    double S_x; //temp matrix
+    Vector2d K_x; //
+    double z_x; //measure value
+    double y_x;
+    double I_x;
+    I_x = 1;
+	
 
+	MatrixXd A_y(2,2);
+    A_y << 1, 0.55,
+		   0, 1;   
+    VectorXd predict_x_y(2);//state
+    predict_x_y(0) = winHeight >> 1;
+	predict_x_y(1) = 0;
+    VectorXd optimize_x_y(2);
+    RowVector2d H_y; //observation
+    H_y(0) = 1;
+	H_y(1) = 0;
+    MatrixXd Q_y(2,2); //process_noise
+    Q_y(0,0) = Q_y(1,1) = 1e-2;
+    double R_y; //measure_noise
+    R_y = 1e-1;
+    
+    MatrixXd P_y(2,2); //cov
+    double S_y; //temp matrix
+    Vector2d K_y; //
+    double z_y; //measure value
+    double y_y;
+    double I_y;
+    I_y = 1;
+
+
+	
     while(1)
     {
         cvSet(img, cvScalar(255, 255, 255, 0));
@@ -59,19 +80,25 @@ int main()
         char buf[256];
         snprintf(buf, 256, "current position :(%3d,%3d)", mousePosition.x, mousePosition.y);
         cvPutText(img, buf, cvPoint(10, 60), &font, CV_RGB(0, 0, 0));
-        z(0) = mousePosition.x;
-        z(1) = mousePosition.y;
-        optimize_x = A * predict_x;
-        P = A * P * A.transpose() + Q;
-        y = z - H * optimize_x;
-        S = H * P * H.transpose() + R;
-        
-        K = P * H.transpose() * S.inverse();
-        predict_x = optimize_x + K * y;
-        //cout << predict_x << endl;
-        P = (I - K * H) * P;
+        z_x = mousePosition.x;
+        optimize_x_x = A_x * predict_x_x;
+        P_x = A_x * P_x * A_x.transpose() + Q_x;
+        y_x = z_x - (H_x * optimize_x_x)(0,0);
+        S_x = (H_x * P_x * H_x.transpose())(0,0) + R_x;
+        K_x = P_x * H_x.transpose()* S_x;
+        predict_x_x = optimize_x_x + K_x * y_x;
+        P_x = (I_x - (K_x * H_x)(0,0)) * P_x;
 
-        CvPoint predictpos = cvPoint((int)predict_x(0),(int)predict_x(1));
+        z_y = mousePosition.y;
+        optimize_x_y = A_y * predict_x_y;
+        P_y = A_y * P_y * A_y.transpose() + Q_y;
+        y_y = z_y - (H_y * optimize_x_y)(0,0);
+        S_y = (H_y * P_y * H_y.transpose())(0,0) + R_y;
+        K_y = P_y * H_y.transpose()* S_y;
+        predict_x_y = optimize_x_y + K_y * y_y;
+        P_y = (I_y - (K_y * H_y)(0,0)) * P_y;
+
+        CvPoint predictpos = cvPoint((int)predict_x_x(0),(int)predict_x_y(0));
         snprintf(buf, 256, "predicted position:(%3d,%3d)", predictpos.x, predictpos.y);
         cvPutText(img, buf, cvPoint(10, 30), &font, CV_RGB(0, 0, 0));
         cvCircle(img, predictpos , 5, CV_RGB(0, 0, 255), 3);
